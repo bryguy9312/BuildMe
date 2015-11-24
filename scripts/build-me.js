2da98902801bf93426cdb4b6275c63eb7e7dfcd3
bryguy9312 <bryguys9312@gmail.com>
2015-11-24 01:37:04 -0800

    Removed Firebase services, added race trait selection functionality

--- scripts/build-me.js
+++ scripts/build-me.js
@@ -1,13 +1,4 @@
 $('document').ready(function() {
-	var validLogout = false;
-	var ref = new Firebase("https://build-me.firebaseio.com");
-	ref.onAuth(authCallback);
-	//checkAuth();
-
-	$('#raceMenu').on('change', function() {
-		changeRaceProfile($("#raceMenu option:selected"));
-	});
-
 	//TODO needs to add character ability mods to calculated number
 	$("#abilityScores input").on("keyup", function() {
 		thisAbil = this.id;
@@ -15,10 +6,6 @@ $('document').ready(function() {
 		$("#"+thisAbil+"Mod").attr('value', modCalc(this.value));
 	});
 
-	$('#logout').click(function() {
-		validLogout = true;
-		ref.unauth();
-	});
 
 	$( "input[type=checkbox]" ).on( "click", function(){
 		thisClass = this.value;
@@ -90,9 +77,71 @@ $('document').ready(function() {
 		character.descrip.age = this.value;
 	});
 
-	$('#addSpell').on('click', function() {
-		$("#spells").append("<div cl>")
-	})
+	$('#raceMenu').on("change", function() {
+		//TODO check for current race, if it is not null then reset or reverse race parameters in order to make way for newly selected race
+		if(this.value == 0 || character.descrip.race != "") {
+			//reset parameters
+		}
+
+
+		if(this.value != 0) {
+			character.descrip.race = this.value;
+			//asynch, do all data manipulation here
+			$.getJSON("raceTraits.json", function(data) {
+				var raceInfo = data[character.descrip.race];
+				console.log(data[character.descrip.race]);
+				//TODO set up a separate trait/feat abilScore modifier value
+				//add race abilScore modifier
+				$.each(raceInfo.abilityScores, function(key, value) {
+					modAbilScore(key, value);
+				});
+				//set race size
+				character.descrip.size = raceInfo.size;
+				//generate and set height
+				character.descrip.height = genHeight(raceInfo.height);
+				//TODO generate weight instead of setting constant
+				//set weight based on race
+				character.descrip.weight = raceInfo.weight;
+				//set movespeed
+				character.descrip.moveSpeed = raceInfo.speed;
+				//TODO generate random starting age?
+				//set age based on race parameters
+				character.descrip.age = raceInfo.age;
+				//clear out character race traits then update with selected race
+				character.stats.raceTraits = {};
+				$.each(raceInfo.traits, function(key, value) {
+					character.stats.raceTraits[key] = value;
+				});
+				//add race proficiencies to character object
+				$.each(raceInfo.proficiencies, function(key, value) {
+					$.each(value.split("-"), function(index, element) {
+						character.stats.proficiencies[key].push(element);
+					})
+				})
+				//add subraces to select menu
+				$.each(raceInfo.subraces, function(index, element) {
+					$("#subraceMenu").append($("<option></option>").attr("value", element).text(element));
+				});
+				$("#subraceMenu").prop("disabled", false);
+			})
+		}
+	});
+
+	$("#subraceMenu").on("change", function() {
+		if(this.value != 0) {
+			$.getJSON("subraceTraits.json", function(data) {
+
+			})
+		}
+	});
+
+	function genHeight(heightString) {
+		var heightArray = heightString.split("-");
+		var max = parseInt(heightArray[1]);
+		var min = parseInt(heightArray[0]);
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
 
 	$('#genAbilScores').on("click", function() {
 		abilScores = "Your ability scores are: ";
@@ -115,31 +164,12 @@ $('document').ready(function() {
 		window.alert(abilScores);
 	});
 
-	function checkAuth() {
-		var authData = ref.getAuth();
-		if (authData) {
-
-		} else {
-			authCallback(authData);
-		}
-	};
-
-	function authCallback(authData) {
-		if (authData) {
-		} else {
-			if (validLogout) {
-				window.location.replace("/index.html");
-			} else {
-				ref.authWithOAuthPopup("facebook", function(error, authData) {
-				  	if (error) {
-				    	console.log("Login Failed!", error);
-				  	} else {
-				    	console.log("Authenticated successfully with payload:", authData);
-				  	}
-				});
-			}
-		}
-	};
+	function modAbilScore(abil, mod) {
+		abil = abil.toLowerCase();
+		console.log(character.stats.abilityScores[abil]);
+		character.stats.abilityScores[abil] += parseInt(mod);
+		console.log(character.stats.abilityScores[abil]);
+	}
 
 	var character = {
 		descrip: {
@@ -155,18 +185,29 @@ $('document').ready(function() {
 			background: "",
 			moveSpeed: 0,
 			size: "",
-			height: "",
+			height: 0,
 			weight: "",
 			deity: "",
 			languages: []
 		},
 		stats: {
-			abilityScores: {},
+			abilityScores: {
+				str: 0,
+				dex: 0,
+				con: 0,
+				int: 0,
+				wis: 0,
+				cha: 0
+			},
 			abilities: {},
 			classFeats: {},
 			raceTraits: {},
 			experience: 0,
-			proficiency: {},
+			proficiencies: {
+				weapon: [],
+				armor: [],
+				tool: []
+			},
 			health: {},
 			proficiencyBonus: 0
 		},
@@ -183,10 +224,4 @@ $('document').ready(function() {
 		return Math.floor((stat - 10) / 2);
 	};
 
-	function changeRaceProfile(raceObj) {
-		if(raceObj.attr('value') != 0) {
-			character.descrip.race = raceObj.text();
-		}
-
-	}
 });
